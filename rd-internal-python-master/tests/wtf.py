c98e2a531a0f969a610a4bc6b8e161036a08952a
ULXOcMYh <babichart@gmail.com>
2021-08-19 22:25:10 +0300

    19.09 home

--- rd-internal-python-master/tests/wtf.py
+++ rd-internal-python-master/tests/wtf.py
@@ -1,8 +1,9 @@
 # import py
 import sys
-sys.path.append('./task_01')
-print(sys.path)
-from task_01.task1 import check_password
-f = task1.check_password
-
-print(f('xx'))
+# sys.path.append('./task_01')
+# print(sys.path)
+# from task_01.task1 import check_password
+# f = task1.check_password
+#
+# print(f('xx'))
+print(sys.version)
