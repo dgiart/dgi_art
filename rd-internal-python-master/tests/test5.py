5f17de8221970f3a38ab9cebde0f7d01a76f0038
ULXOcMYh <babichart@gmail.com>
2021-08-20 17:11:41 +0300

    task11 and test11 done

---
+++ rd-internal-python-master/tests/test5.py
@@ -0,0 +1,13 @@
+import pytest
+import sys
+
+sys.path.insert(0, '..')
+from task_05 import task5
+
+
+def test_mults():
+    assert task5.mults(3, 5, 10) == 23
+    assert task5.mults(5, 3, 10) == 23
+    assert task5.mults(3, 2, 10) == 32
+    assert task5.mults(7, 8, 50) == 364
+
---
+++ rd-internal-python-master/tests/test_11.py
@@ -0,0 +1,23 @@
+import pytest
+import sys
+
+sys.path.insert(0, '..')
+from task_11 import task11
+
+summa = 0
+l1 = [1, 2, 3]
+l2 = [1, [5, 6, [13, 5]], 2, 3, [2, [-5, 6, [-7, [], 8]]], -50]
+l3 = [1, [], 2, [-19, 700, 0, [90, 33, [18, 77, [0, ], -2], 11, 16], -100]]
+l4 = [[3, 4]]
+
+
+def test_sum_of_elements1():
+    assert task11.sum_of_elements(l1) == 6
+
+
+def test_sum_of_elements2():
+    assert task11.sum_of_elements(l2) == -11
+
+
+def test_sum_of_elements3():
+    assert task11.sum_of_elements(l3) == 827
